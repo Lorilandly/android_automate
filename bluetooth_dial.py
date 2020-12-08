5cfbf836ecff2fefbb59b3f1ca32fb751ffc2658
haoan <place@holder.com>
2020-12-08 15:06:10 +0800

    增加连接已配对设备功能

--- bluetooth_dial.py
+++ bluetooth_dial.py
@@ -10,20 +10,45 @@ except Exception:
 	print("无法连接到设备\n请检查连接")
 	exit()
 
-if app in d.shell("pm list packages").output:
-	d.app_start(app, stop=True)
-else:
-	if subprocess.run("adb install ./EmbeddedKitchenSinkApp.apk",shell=True).returncode == 0:
-		d.app_start(app, stop=True)
-	else:
+if not app in d.shell("pm list packages").output:
+	if not subprocess.run("adb install ./EmbeddedKitchenSinkApp.apk",shell=True).returncode == 0:
 		print("unknown error")
 		exit()
 
+d.app_start("com.android.car.settings", stop=True)
+d(text="Settings").wait(timeout=10)
+d(text="More").click()
+d(scrollable=True).scroll.to(text="Bluetooth")
+d(text="Bluetooth").click()
+# 蓝牙开
+d.swipe(1800,115,1900,115,0.05)
+if not d(text="Bluetooth will turn on to pair").wait_gone(timeout=60):
+	print("蓝牙开启失败")
+	exit()
+
+if d(text="Paired devices").exists():
+	if not d(text="Connected").exists:
+		# 拉个已连接设备的表
+		paired = d.xpath('//*[@resource-id="com.android.car.settings:id/nested_recycler_view_layout"]/androidx.recyclerview.widget.RecyclerView[1]/android.widget.LinearLayout').all()
+		choice = 1
+		if len(paired)>2:
+			print(f"发现{len(paired)-1}个设备")
+			choice = int(input("选择第几个> "))
+		# 点击连接设备
+		paired[choice].click()
+		if not d(text="Connected").wait(timeout=30):
+			print("Error connecting")
+			exit()
+else:
+	# d(text="Pair new device").click()
+	print("No paired device")
+	print("请先手动连接一个设备")
+	exit()
+
+d.app_start(app, stop=True)
 d(text="BLUETOOTH HEADSET").click()
 d(text="PICK DEVICE").click()
-if not d(text="Connected").click_exists(timeout=5):
-	print("Connect a device manually first!")
-	d.press("back")
+d(text="Connected").click_exists(timeout=3)
 d(text="Enter number").click()
 d.send_keys("10000")
 d(text="START CALL").click()
